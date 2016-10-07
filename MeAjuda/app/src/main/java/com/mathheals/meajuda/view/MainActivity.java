143a4e5c13d8ded6b4151b28368f0df52c5c7b8a
Izabela Cardoso <cizabelacristina@gmail.com>
2016-10-06 21:36:33 -0300

    Adding link to loginActivity and changing logo

--- MeAjuda/app/src/main/java/com/mathheals/meajuda/view/MainActivity.java
+++ MeAjuda/app/src/main/java/com/mathheals/meajuda/view/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mathheals.meajuda.view;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -25,7 +26,6 @@ import com.mathheals.meajuda.model.Category;
 import com.mathheals.meajuda.presenter.CategoryPresenter;
 
 import org.json.JSONException;
-import org.w3c.dom.Text;
 
 import java.util.Vector;
 
@@ -167,13 +167,15 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if(id == R.id.action_settings){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+
             return true;
         }
 
