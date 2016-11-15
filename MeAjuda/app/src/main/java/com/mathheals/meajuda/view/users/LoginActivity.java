f4d2444ab62402c2fcd1574688a34a02ba82474c
Izabela Cardoso <cizabelacristina@gmail.com>
2016-11-15 17:27:17 -0200

    Set visibility of itens according to user login

--- MeAjuda/app/src/main/java/com/mathheals/meajuda/view/users/LoginActivity.java
+++ MeAjuda/app/src/main/java/com/mathheals/meajuda/view/users/LoginActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -64,6 +65,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 else{
                     SharedPreferences session = PreferenceManager.
                             getDefaultSharedPreferences(getApplicationContext());
+
                     userPresenter.createLoginSession(status, getApplicationContext(), session);
 
                     Toast.makeText(getBaseContext(), getResources().getString
